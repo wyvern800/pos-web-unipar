afbe80c851bb5701dc8403043efe513a87d6c958
wyvern800 <matheusguiferreira@hotmail.com>
2021-04-25 20:20:10 -0300

    Funcionando, ainda com bugs

--- atividade-24-04-2021/js/app.js
+++ atividade-24-04-2021/js/app.js
@@ -1,6 +1,6 @@
 function dragStart(ev) {
   ev.dataTransfer.effectAllowed = "move";
-  ev.dataTransfer.setData("Text", ev.target.getAttribute("id"));
+  ev.dataTransfer.setData("id", ev.target.getAttribute("id"));
   ev.dataTransfer.setDragImage(ev.target, 0, 0);
   updateValues();
   return true;
@@ -13,32 +13,9 @@ function dragOver(ev) {
   return false;
 }
 function dragDrop(ev) {
-  var src = ev.dataTransfer.getData("Text");
+  ev.preventDefault();
+  var src = ev.dataTransfer.getData("id");
   ev.target.appendChild(document.getElementById(src));
-  ev.stopPropagation();
   updateValues();
   return false;
-}
-
-function updateValues() {
-  var total_items_comp = document.getElementById("total-itens");
-  var total_comp = document.getElementById("valor-total");
-
-  // Counts and set the items in cart
-  var prods_in_cart = $("#carrinho .prod").length;
-  total_items_comp.innerHTML = ""+prods_in_cart;
-
-  var total = 0;
-  for (i = 0; i < prods_in_cart; i++) {
-    var individual_price = $("input[type='text'][name='valor']");
-    var quantity = $("input[type='number'][name='qtde'");
-    var price = (individual_price.val() * quantity.val());
-    total += price;
-
-    console.log("Preço individual: "+individual_price.val())
-    console.log("Qtde: "+quantity.val())
-    console.log(total)
-  }
-
-  total_comp.innerHTML = total;
 }
